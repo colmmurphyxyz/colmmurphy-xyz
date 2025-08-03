0ae0a83c61163f599992b661fe27ab19e4c12442
Colm Murphy <colm@colmmurphy.xyz>
2025-08-03 14:06:02 +0100

    remove react dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [],
 
   adapter: node({
     mode: 'standalone'
