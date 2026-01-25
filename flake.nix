6ab8d6bbf6f7d7413f6cc53c21e77880054fc36b
Colm Murphy <colmmurphy016@gmail.com>
2026-01-25 14:52:44 +0000

    upgrade flake to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "AstroJS static site";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -53,7 +53,7 @@
           installPhase = ''
             mkdir -p $out
             cp -r dist node_modules $out/
-            
+
             mkdir -p $out/bin
             cat > $out/bin/colmmurphy-xyz-frontend <<EOF
             #!${pkgs.bash}/bin/bash
