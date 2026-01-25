3b2ed891cbbf1c70b97271cd079d6745c4b04cd2
Colm Murphy <colmmurphy016@gmail.com>
2026-01-25 13:26:23 +0000

    nix: format

--- flake.nix
+++ flake.nix
@@ -6,21 +6,28 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         nodejs = pkgs.nodejs_20;
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           name = "astro-dev-shell";
-          
+
           buildInputs = [
             nodejs
             pkgs.git
             pkgs.nodePackages.npm
           ];
-          
+
           shellHook = ''
             echo "activated";
           '';
@@ -28,18 +35,18 @@
 
         packages.default = pkgs.buildNpmPackage {
           name = "colmmurphy-xyz-frontend";
-          
+
           buildInputs = [
             nodejs
             pkgs.nodePackages.npm
           ];
-          
+
           src = self;
 
           npmDeps = pkgs.importNpmLock {
             npmRoot = ./.;
           };
-          
+
           npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 
           installPhase = ''
@@ -48,5 +55,6 @@
             cp -r node_modules/ $out/;
           '';
         };
-      });
+      }
+    );
 }
